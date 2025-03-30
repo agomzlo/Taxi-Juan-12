e46d6096de373a34f70290681dc3a81c3439ed1f
agomzlo <agomzlodev@gmail.com>
2025-03-30 15:56:48 +0200

    feat: First version for review

---
+++ src/utils.js
@@ -0,0 +1,76 @@
+import Buildings from "./icons/Buildings.astro";
+import Airport from "./icons/Airport.astro"
+import Calendar from "./icons/Calendar.astro";
+import MapPin from "./icons/MapPin.astro";
+import Train from "./icons/Train.astro";
+import Business from "./icons/Business.astro";
+import Museum from "./icons/Museum.astro";
+import WiFi from "./icons/WiFi.astro";
+import ClockPin from "./icons/ClockPin.astro";
+import SecurePin from "./icons/SecurePin.astro";
+
+export const services = [
+    {
+      image: "Buildings",
+      title: "Traslados locales y provinciles",
+      description: "Viajes sin límite a cualquier localidad de la provincia de Málaga"
+    },
+    {
+      image: "Airport",
+      title: "Transfers al Aeropuerto de Málaga",
+      description: "Servicio de recogida y traslado al aeropuerto con total puntualidad"
+    },
+    {
+      image: "SecurePin",
+      title: "Seguridad durante el viaje",
+      description: "Viajes con tranquilidad gracias a nuestros conductores profesionales"
+    },
+    {
+      image: "WiFi",
+      title: "WiFi gratuito",
+      description: "Disfruta de conexión a internet gratuita para que puedas navegar durante el viaje"
+    },
+    {
+      image: "Train",
+      title: "Conexión con estaciones de transporte",
+      description: "Servicio de recogida y traslado a estaciones de transporte público"
+    },
+    {
+      image: "Calendar",
+      title: "Reservas anticipadas",
+      description: "Posibilidad de programar el servicio con antelación para mayor comodidad"
+    },
+    {
+      image: "MapPin",
+      title: "Viajes de larga distancia",
+      description: "Viajes a cualquier punto de España con tarifas competitivas"
+    },
+    {
+      image: "Business",
+      title: "Transporte para empresas y eventos",
+      description: "Servicio adaptado a necesidades empresariales y traslados grupales"
+    },
+    {
+      image: "Museum",
+      title: "Taxi turístico",
+      description: "Recorridos por la comarca de la Axarquía para turistas que deseen conocer la zona con comodidad"
+    },
+    {
+      image: "ClockPin",
+      title: "Puntualidad asegurada",
+      description: "Aseguramos que llegues a tu destino sin demoras y con total confianza"
+    }
+  ];
+
+export const icons = {
+    Buildings,
+    Airport,
+    Calendar,
+    MapPin,
+    Train,
+    Business,
+    Museum,
+    WiFi,
+    ClockPin,
+    SecurePin
+  };
